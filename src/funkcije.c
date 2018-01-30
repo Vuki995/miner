9910c1f751949a12a074738fe1e82c861c8a97c0
Vuki995 <vuki995@hotmail.rs>
2018-01-30 20:16:18 +0100

    dodata funkcija za ispisivanje teksta na ekranu

--- src/funkcije.c
+++ src/funkcije.c
@@ -1,4 +1,4 @@
-#include"header.h" 
+#include"header.h"
 #include<stdlib.h>
 #include<GL/glut.h>
 #include<stdio.h>
@@ -35,6 +35,31 @@ for(i=0;i<20;i++){
 	else
 		i--;
 	}
-
+for(i=0;i<Bnum;i++){
+	j=rand()%10;
+	k=rand()%10;
+	l=rand()%10;
+	if(j!=4&&k!=0&&l!=4&&a[j][k][l].tip!=2&&a[j][k][l].tip!=3){
+		a[j][k][l].tip=3;
+	}
+	else
+		i--;
+	}
 }
 
+
+
+void RenderString(float x, float y, void *font, const char* string, float r, float g, float b){ /*ispis teksta*/
+  GLfloat emission_coeffs[] = { r, g, b, 1 };
+  glRasterPos3f(x, y, 1);
+  glutBitmapString(font, string);
+  glMaterialfv(GL_FRONT, GL_EMISSION, emission_coeffs);
+}
+
+void ispisi(char* d,int j, float a, float b){
+char c[10];
+RenderString(a-0.2,b, GLUT_BITMAP_HELVETICA_18,d,1,1,1);
+if(j>=0){
+sprintf(c, "%d", j);
+RenderString(a+0.2,b, GLUT_BITMAP_HELVETICA_18,c,1,1,1);}
+}
