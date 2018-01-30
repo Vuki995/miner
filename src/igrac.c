19af5acfd84b36aa07ba66b49351eadb4d8cbce7
Vuki995 <vuki995@hotmail.rs>
2018-01-30 20:21:46 +0100

    dodata funkcija koja ogranicava vidljivost

--- src/igrac.c
+++ src/igrac.c
@@ -1,9 +1,9 @@
-#include"header.h" 
+#include"header.h"
 #include<stdlib.h>
 #include<GL/glut.h>
 #include<stdio.h>
 #include <GL/freeglut.h>
-igrac spawn(void){ /*postavlja pocetne vrednosti igraca*/
+igrac spawn(void){
     igrac a;
     a.i=4;
     a.j=0;
@@ -15,15 +15,16 @@ igrac spawn(void){ /*postavlja pocetne vrednosti igraca*/
     a.y1=4.5;
     a.z1=0.5;
     a.korak=0;
-    a.stanje=1;
+    a.stanje=0;
     a.smer=0;
     a.i1=0;
     a.j1=0;
     a.k1=0;
+    a.score=0;
 return a;
 }
-/*funkcija kretanja, u zavisnosti od pritisnutog dugmeta, orijentacije i pozicije, funkcija odredjuje da li igrac moze da se krece*/
-igrac pokret(igrac a, char c,kocka k[brred][brred][brred]){ 
+
+igrac pokret(igrac a, char c,kocka k[brred][brred][brred], int *m, bot b[Bnum], int *count){ 
     a.x1=a.x;
     a.y1=a.y;
     a.z1=a.z;
@@ -32,13 +33,7 @@ if(c=='w'){
         if(k[a.i][a.j-1][a.k].prokopano==1)
 	    a.j1=-1;
 	else if(k[a.i][a.j-1][a.k].prokopano==0){
-		k[a.i][a.j-1][a.k].health--;
-		if(k[a.i][a.j-1][a.k].health==0){
-			if(k[a.i][a.j-1][a.k].tip==2){
-				a.score=a.score+1;}
-			k[a.i][a.j-1][a.k].prokopano=1;
-			osvetli(k,a.i,a.j-1,a.k);
-		}
+		pMove(k, a.i, a.j-1, a.k, m,count, b, &a);
 	}
     }
 
@@ -49,13 +44,7 @@ if(c=='s'){
         if(k[a.i][a.j+1][a.k].prokopano==1)
 	    a.j1=1;
 	else if(k[a.i][a.j+1][a.k].prokopano==0){
-		k[a.i][a.j+1][a.k].health--;
-		if(k[a.i][a.j+1][a.k].health==0){
-			if(k[a.i][a.j+1][a.k].tip==2){
-				a.score=a.score+1;}
-			k[a.i][a.j+1][a.k].prokopano=1;
-			osvetli(k,a.i,a.j+1,a.k);
-		}
+		pMove(k, a.i, a.j+1, a.k, m,count, b, &a);
 	}
     }
 return a;
@@ -66,13 +55,7 @@ switch(a.smer){
    		     if(k[a.i][a.j][a.k+1].prokopano==1)
 			    a.k1=1;
 		     else if(k[a.i][a.j][a.k+1].prokopano==0){
-			k[a.i][a.j][a.k+1].health--;
-			    if(k[a.i][a.j][a.k+1].health==0){
-				if(k[a.i][a.j][a.k+1].tip==2){
-				     a.score=a.score+1;}
-				k[a.i][a.j][a.k+1].prokopano=1;
-				osvetli(k,a.i,a.j,a.k+1);
-		     	     }
+			pMove(k, a.i, a.j, a.k+1, m,count, b, &a);
 		      }
   		  }
 		return a;
@@ -82,13 +65,7 @@ switch(a.smer){
    		     if(k[a.i][a.j][a.k-1].prokopano==1)
 			    a.k1=-1;
 		     else if(k[a.i][a.j][a.k-1].prokopano==0){
-			k[a.i][a.j][a.k-1].health--;
-			    if(k[a.i][a.j][a.k-1].health==0){
-				if(k[a.i][a.j][a.k-1].tip==2){
-				     a.score=a.score+1;}
-				k[a.i][a.j][a.k-1].prokopano=1;
-				osvetli(k,a.i,a.j,a.k-1);
-		     	     }
+			pMove(k, a.i, a.j, a.k-1, m,count, b, &a);
 		      }
 		  }
 		return a;
@@ -98,13 +75,7 @@ switch(a.smer){
    		     if(k[a.i+1][a.j][a.k].prokopano==1)
 			    a.i1=1;
 		     else if(k[a.i+1][a.j][a.k].prokopano==0){
-			k[a.i+1][a.j][a.k].health--;
-			    if(k[a.i+1][a.j][a.k].health==0){
-				if(k[a.i+1][a.j][a.k].tip==2){
-				     a.score=a.score+1;}
-				k[a.i+1][a.j][a.k].prokopano=1;
-				osvetli(k,a.i+1,a.j,a.k);
-		     	     }
+			pMove(k, a.i+1, a.j, a.k, m,count, b, &a);
 		      }
   		  }
 		return a;
@@ -114,13 +85,7 @@ switch(a.smer){
    		     if(k[a.i-1][a.j][a.k].prokopano==1)
 			    a.i1=-1;
 		     else if(k[a.i-1][a.j][a.k].prokopano==0){
-			k[a.i-1][a.j][a.k].health--;
-			    if(k[a.i-1][a.j][a.k].health==0){
-				if(k[a.i-1][a.j][a.k].tip==2){
-				     a.score=a.score+1;}
-				k[a.i-1][a.j][a.k].prokopano=1;
-				osvetli(k,a.i-1,a.j,a.k);
-		     	     }
+			pMove(k, a.i-1, a.j, a.k, m,count, b, &a);
 		      }
   		  }
 		return a;
@@ -130,13 +95,7 @@ switch(a.smer){
    		     if(k[a.i][a.j][a.k+1].prokopano==1)
 			    a.k1=1;
 		     else if(k[a.i][a.j][a.k+1].prokopano==0){
-			k[a.i][a.j][a.k+1].health--;
-			    if(k[a.i][a.j][a.k+1].health==0){
-				if(k[a.i][a.j][a.k+1].tip==2){
-				     a.score=a.score+1;}
-				k[a.i][a.j][a.k+1].prokopano=1;
-				osvetli(k,a.i,a.j,a.k+1);
-		     	     }
+			pMove(k, a.i, a.j, a.k+1, m,count, b, &a);
 		      }
   		  }
 		return a;
@@ -146,13 +105,7 @@ switch(a.smer){
    		     if(k[a.i][a.j][a.k-1].prokopano==1)
 			    a.k1=-1;
 		     else if(k[a.i][a.j][a.k-1].prokopano==0){
-			k[a.i][a.j][a.k-1].health--;
-			    if(k[a.i][a.j][a.k-1].health==0){
-				if(k[a.i][a.j][a.k-1].tip==2){
-				     a.score=a.score+1;}
-				k[a.i][a.j][a.k-1].prokopano=1;
-				osvetli(k,a.i,a.j,a.k-1);
-		     	     }
+			pMove(k, a.i, a.j, a.k-1, m,count, b, &a);
 		      }
   		  }
 		return a;
@@ -162,13 +115,7 @@ switch(a.smer){
    		     if(k[a.i+1][a.j][a.k].prokopano==1)
 			    a.i1=1;
 		     else if(k[a.i+1][a.j][a.k].prokopano==0){
-			k[a.i+1][a.j][a.k].health--;
-			    if(k[a.i+1][a.j][a.k].health==0){
-				if(k[a.i+1][a.j][a.k].tip==2){
-				     a.score=a.score+1;}
-				k[a.i+1][a.j][a.k].prokopano=1;
-				osvetli(k,a.i+1,a.j,a.k);
-		     	     }
+			pMove(k, a.i+1, a.j, a.k, m,count, b, &a);
 		      }
   		  }
 		return a;
@@ -178,13 +125,7 @@ switch(a.smer){
    		     if(k[a.i-1][a.j][a.k].prokopano==1)
 			    a.i1=-1;
 		     else if(k[a.i-1][a.j][a.k].prokopano==0){
-			k[a.i-1][a.j][a.k].health--;
-			    if(k[a.i-1][a.j][a.k].health==0){
-				if(k[a.i-1][a.j][a.k].tip==2){
-				     a.score=a.score+1;}
-				k[a.i-1][a.j][a.k].prokopano=1;
-				osvetli(k,a.i-1,a.j,a.k);
-		     	     }
+			pMove(k, a.i-1, a.j, a.k, m,count, b, &a);
 		      }
   		  }
 		return a;
@@ -192,19 +133,183 @@ switch(a.smer){
 
 }
 }
-void osvetli(kocka k[brred][brred][brred],int a, int b, int c){/*osvetli polja oko prosledjene lokacije*/
- 
-if(a<9)
-	k[a+1][b][c].vidjeno=1;
-if(a>0)
-	k[a-1][b][c].vidjeno=1;
-if(b<9)
-	k[a][b+1][c].vidjeno=1;
-if(b>0)
-	k[a][b-1][c].vidjeno=1;
-if(c<9)
-	k[a][b][c+1].vidjeno=1;
-if(c>0)
-	k[a][b][c-1].vidjeno=1;
+void pMove(kocka k[brred][brred][brred], int i, int j, int l, int *m,int * count, bot b[Bnum], igrac * a){
+	if(k[i][j][l].tip==3){
+		b[*m]=probudi(k[i][j][l],i,j,l);
+		k[i][j][l].health=0;
+		k[i][j][l].prokopano=1;
+		*m=*m+1;
+		*count=*count+1;
+	}
+	k[i][j][l].health--;
+	if(k[i][j][l].health==0){
+		if(k[i][j][l].tip==2){
+			a->score=a->score+100;}
+		k[i][j][l].prokopano=1;
+	}
+}
+
+void osvetli(kocka k[brred][brred][brred],int a, int b, int c, int vidi){
+int vid=0;
+ if(vidi){
+vid=1;}
+k[a][b][c].vidjeno=vid;
+if(a<9){
+	k[a+1][b][c].vidjeno=vid;
+		if(k[a+1][b][c].prokopano){
+			if(a+1<9){
+				k[a+2][b][c].vidjeno=vid;
+				if(k[a+2][b][c].prokopano){
+					if(a+2<9)
+						k[a+3][b][c].vidjeno=vid;
+					if(b<9)
+						k[a+2][b+1][c].vidjeno=vid;
+					if(b>0)
+						k[a+2][b-1][c].vidjeno=vid;
+				}
+			}
+			if(b<9)
+				k[a+1][b+1][c].vidjeno=vid;
+			if(b>0)
+				k[a+1][b-1][c].vidjeno=vid;
+		}
+	}
+if(a>0){
+	k[a-1][b][c].vidjeno=vid;
+		if(k[a-1][b][c].prokopano){
+			if(a-1>0){
+				k[a-2][b][c].vidjeno=vid;
+				if(k[a-2][b][c].prokopano){
+					if(a-2>0)
+						k[a-3][b][c].vidjeno=vid;
+					if(b<9)
+						k[a-2][b+1][c].vidjeno=vid;
+					if(b>0)
+						k[a-2][b-1][c].vidjeno=vid;
+				}
+			}
+			if(b<9)
+				k[a-1][b+1][c].vidjeno=vid;
+			if(b>0)
+				k[a-1][b-1][c].vidjeno=vid;
+		}
+	}
+if(b<9){
+	k[a][b+1][c].vidjeno=vid;
+		if(k[a][b+1][c].prokopano){
+			if(b+1<9){
+				k[a][b+2][c].vidjeno=vid;
+				if(k[a][b+2][c].prokopano){
+					if(b+2<9)
+						k[a][b+3][c].vidjeno=vid;
+					if(a<9)
+						k[a+1][b+2][c].vidjeno=vid;
+					if(a>0)
+						k[a-1][b+2][c].vidjeno=vid;
+					if(c<9)
+						k[a][b+2][c+1].vidjeno=vid;
+					if(c>0)
+						k[a][b+2][c-1].vidjeno=vid;
+				}
+			}
+			if(a<9)
+				k[a+1][b+1][c].vidjeno=vid;
+			if(a>0)
+				k[a-1][b+1][c].vidjeno=vid;
+			if(c<9)
+				k[a][b+1][c+1].vidjeno=vid;
+			if(c>0)
+				k[a][b+1][c-1].vidjeno=vid;
+		}
+	}
+if(b>0){
+	k[a][b-1][c].vidjeno=vid;
+		if(k[a][b-1][c].prokopano){
+			if(b-1>0){
+				k[a][b-2][c].vidjeno=vid;
+				if(k[a][b-2][c].prokopano){
+					if(b-2>0)
+						k[a][b-3][c].vidjeno=vid;
+					if(a<9)
+						k[a+1][b-2][c].vidjeno=vid;
+					if(a>0)
+						k[a-1][b-2][c].vidjeno=vid;
+					if(c<9)
+						k[a][b-2][c+1].vidjeno=vid;
+					if(c>0)
+						k[a][b-2][c-1].vidjeno=vid;
+				}
+			}
+			if(a<9)
+				k[a+1][b-1][c].vidjeno=vid;
+			if(a>0)
+				k[a-1][b-1][c].vidjeno=vid;
+			if(c<9)
+				k[a][b-1][c+1].vidjeno=vid;
+			if(c>0)
+				k[a][b-1][c-1].vidjeno=vid;
+		}
+	}
+if(c<9){
+	k[a][b][c+1].vidjeno=vid;
+		if(k[a][b][c+1].prokopano){
+			if(c+1<9){
+				k[a][b][c+2].vidjeno=vid;
+				if(k[a][b][c+2].prokopano){
+					if(c+2<9)
+						k[a][b][c+3].vidjeno=vid;
+					if(b<9)
+						k[a][b+1][c+2].vidjeno=vid;
+					if(b>0)
+						k[a][b-1][c+2].vidjeno=vid;
+				}
+			}
+			if(b<9)
+				k[a][b+1][c+1].vidjeno=vid;
+			if(b>0)
+				k[a][b-1][c+1].vidjeno=vid;
+		}
+	}
+if(c>0){
+	k[a][b][c-1].vidjeno=vid;
+		if(k[a][b][c-1].prokopano){
+			if(c-1>0){
+				k[a][b][c-2].vidjeno=vid;
+				if(k[a][b][c-2].prokopano){
+					if(c-2>0)
+						k[a][b][c-3].vidjeno=vid;
+					if(b<9)
+						k[a][b+1][c-2].vidjeno=vid;
+					if(b>0)
+						k[a][b-1][c-2].vidjeno=vid;
+				}
+			}
+			if(b<9)
+				k[a][b+1][c-1].vidjeno=vid;
+			if(b>0)
+				k[a][b-1][c-1].vidjeno=vid;
+		}
+	}
 return;
+}
+void korak(igrac * a,kocka kocke[brred][brred][brred]){
+if(a->i1!=0||a->j1!=0||a->k1!=0){
+	if(a->korak<MAXK){
+	    a->x=(a->x*100-(a->k1*100/MAXK))/100;   
+	    a->y=(a->y*100-(a->j1*100/MAXK))/100;
+	    a->z=(a->z*100-(a->i1*100/MAXK))/100;
+	    a->korak++;
+	} 
+	if(a->korak==MAXK){	
+	    osvetli(kocke,a->i, a->j, a->k, 0);
+	    a->x=a->x1-a->k1;
+	    a->y=a->y1-a->j1;
+	    a->z=a->z1-a->i1;
+	    a->i=a->i+a->i1;
+	    a->j=a->j+a->j1;
+	    a->k=a->k+a->k1; 
+	    a->i1=a->j1=a->k1=a->korak=0;
+	    osvetli(kocke,a->i, a->j, a->k, 1);
+	}
+    }
 }
